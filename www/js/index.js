bc507bfaab612276494b699d0600a1b41d663625
tibujr <maycol_to@hotmail.com>
2014-11-05 16:41:34 -0500

    haber

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         //llamar location
-        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
+        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError,{enableHighAccuracy:true});
     },
 
     onSuccess: function(position) {
